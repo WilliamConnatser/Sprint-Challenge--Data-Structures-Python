8b375579c9ec4eb1468b9ee5695c7bee98d7fb45
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-07-26 10:34:47 -0500

    Completed ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,8 +4,29 @@ class RingBuffer:
     self.current = 0
     self.storage = [None]*capacity
 
+  '''
+    The append method adds elements to the buffer.
+  '''
   def append(self, item):
-    pass
+    if None in self.storage:
+      self.storage[self.storage.index(None)] = item
+      self.current += 1
+    else:
+      if self.current == self.capacity:
+        self.current = 0
+      self.storage[self.current] = item
+      self.current += 1
 
+
+  '''
+    The get method returns all of the elements in the buffer in
+    a list in their given order. It should not return any None
+    values in the list even if they are present in the ring buffer.
+  '''
   def get(self):
-    pass
+    if None in self.storage:
+      result = self.storage.copy()
+      while None in result:
+        result.pop()
+      return result
+    return self.storage
